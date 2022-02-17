21912e0e5c1c02de5d80a4c6ee2bfdd530036955
Andrew Read <aj.read13@gmail.com>
2022-02-16 19:46:52 -0500

    adding styles to about page.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import MyPic from '../../assets/images/me22.jpg'
+import '../styles/About.css'
 
 export default function About() {
   return (
     <div>
       <h1>About Me</h1>
+      <article id="first">
+      <figure className="kaiden">
+        <img className="potrait" src={MyPic} alt="Graphic with Coming Soon text"/>
+        <figcaption className="passage"> "To improve is to change; to be perfect is to change often." -Winston Churchill
+        </figcaption>
+      </figure>
+    </article>
       <p>
         Full Stack Web Dev, former U.S. Army veteran, gamer, and family man.  
-
-        "To improve is to change; to be perfect is to change often."
-                    -Winston Churchill
       </p>
+
     </div>
   );
 }
